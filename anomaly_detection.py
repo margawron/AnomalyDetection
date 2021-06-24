4c00d81173e1996828b76fba1fe6ce865d9be531
Marcel Gawron <marcel.gawron@outlook.com>
2021-06-25 00:54:55 +0200

    Changed how data is processed

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -3,9 +3,11 @@ https://github.com/keras-team/keras-io/blob/master/examples/vision/video_classif
 https://theailearner.com/2018/10/15/creating-video-from-images-using-opencv-python/
 """ 
 
+from numpy.linalg.linalg import norm
 import tensorflow.keras as keras
 import cv2
 import numpy as np
+from tensorflow.python.keras.backend import reshape
 keras.backend.clear_session()
 
 avenue_dataset = []
@@ -27,12 +29,38 @@ for video in videos:
     ret, frame = cap.read()
     while ret:
         image = cv2.cvtColor(frame, cv2.COLOR_RGBA2GRAY)
-        image = np.resize(image, newsize)
-        image = image * (255.0/image.max())
+        image = cv2.resize(image, (227, 227), interpolation=cv2.INTER_AREA)
+        image = cv2.normalize(image, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        image = image.reshape(227, 227, 1)
         videos_buffer.append(image)
         ret, frame = cap.read()
     cap.release()
     break
+videos_buffer = np.clip(videos_buffer, 0, 1)
+
+data = []
+num = len(videos_buffer)
+for idx in range(num-10):
+    frame_batch = videos_buffer[idx:idx+10]
+    if len(frame_batch) == 10:
+        frame_batch = np.array(frame_batch)
+        data.append(frame_batch)
+
+for idx in range(num//2):
+    frame_batch = videos_buffer[idx:2*idx:2]
+    if len(frame_batch) == 10:
+        frame_batch = np.array(frame_batch)
+        data.append(frame_batch)
+
+for idx in range(num//3):
+    frame_batch = videos_buffer[idx:3*idx:3]
+    if len(frame_batch) == 10:
+        frame_batch = np.array(frame_batch)
+        data.append(frame_batch)
+
+
+data = np.array(data)
+
 """
     counter = 0
     strideCounter = 0
@@ -47,43 +75,38 @@ for video in videos:
         strideCounter= strideCounter+1
 """
 
-
-from tensorflow.keras.layers import ConvLSTM2D, Conv2D, Conv2DTranspose, InputLayer
+from tensorflow.keras.layers import ConvLSTM2D, Conv2D, Conv2DTranspose, Input
 from tensorflow.keras.layers import BatchNormalization, TimeDistributed, Activation
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Model
 
-model = Sequential()
-
-model.add(InputLayer(input_shape=(10, 227, 227, 1)))
-
+inputs = Input(shape=(10, 227, 227, 1))
+#        norm_prediction = model.predict_on_batch(X[i].reshape(1, 160, 240, 1))
 # pierwsza warstwa
-model.add(TimeDistributed(Conv2D(filters=128, kernel_size=(11,11), strides=(4,4))))
-model.add(TimeDistributed(BatchNormalization()))
-model.add(TimeDistributed(Activation('relu')))
+
+conv_11 = TimeDistributed(Conv2D(filters=128, kernel_size=(11,11), padding='same', strides=(4,4)))(inputs)
+conv_11 = TimeDistributed(BatchNormalization())(conv_11)
+conv_11 = TimeDistributed(Activation('relu'))(conv_11)
 
 # druga warstwa
-model.add(TimeDistributed(Conv2D(filters=64, kernel_size=(5,5), strides=(2,2))))
-model.add(TimeDistributed(BatchNormalization()))
-model.add(TimeDistributed(Activation('relu')))
+conv_5 = TimeDistributed(Conv2D(filters=64, kernel_size=(5,5), padding='same', strides=(2,2)))(conv_11)
+conv_5 = TimeDistributed(BatchNormalization())(conv_5)
+conv_5 = TimeDistributed(Activation('relu'))(conv_5)
 
+autoencoder = ConvLSTM2D(filters=64, padding='same', return_sequences=True, kernel_size=(3,3))(conv_5)
+autoencoder = ConvLSTM2D(filters=32, padding='same', return_sequences=True, kernel_size=(3,3))(autoencoder)
+autoencoder = ConvLSTM2D(filters=64, padding='same', return_sequences=True, kernel_size=(3,3))(autoencoder)
 
-model.add(ConvLSTM2D(filters=64, padding='same', return_sequences=True, kernel_size=(3,3)))
-model.add(ConvLSTM2D(filters=32, padding='same', return_sequences=True, kernel_size=(3,3)))
-model.add(ConvLSTM2D(filters=64, padding='same', return_sequences=True, kernel_size=(3,3)))
+deconv_5 = TimeDistributed(Conv2DTranspose(filters=128, kernel_size=(5,5), padding='same', strides=(2,2)))(autoencoder)
+deconv_5 = TimeDistributed(BatchNormalization())(deconv_5)
+deconv_5 = TimeDistributed(Activation('relu'))(deconv_5)
 
-model.add(TimeDistributed(Conv2DTranspose(filters=128, kernel_size=(5,5), strides=(2,2))))
-model.add(TimeDistributed(BatchNormalization()))
-model.add(TimeDistributed(Activation('relu')))
+deconv_11 = TimeDistributed(Conv2DTranspose(filters=1, kernel_size=(11,11), padding='same', strides=(4,4)))(deconv_5)
+deconv_11 = TimeDistributed(BatchNormalization())(deconv_11)
+deconv_11 = TimeDistributed(Activation('relu'))(deconv_11)
 
-model.add(TimeDistributed(Conv2DTranspose(filters=1, kernel_size=(11,11), strides=(4,4))))
-model.add(TimeDistributed(BatchNormalization()))
-model.add(TimeDistributed(Activation('relu')))
+outputs = TimeDistributed(Conv2DTranspose(filters=1, kernel_size=(227,227), padding='same', strides=(4,4)))(deconv_11)
 
-model.add(TimeDistributed(Conv2DTranspose(filters=1, kernel_size=(227,227), strides=(4,4))))
-
-
-model.summary()
-
+model = Model(inputs=inputs, outputs=outputs)
 
 model.compile(
     optimizer='Adam',
@@ -94,22 +117,26 @@ model.compile(
 from tensorflow.keras.callbacks import ModelCheckpoint
 #checkpoint
 filepath = "weights-improvement-{epoch:02d}-{val_accuracy:.2f}.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1,save_best_only=True, mode='max')
+checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
 callback_list = [checkpoint]
-
 
 """
     ZMIENNE POMOCNICZE
 
 """
-epochs = 100
-
+epochs = 10
 
 """
     TODO zmienić dane trenujące
 """
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+model.summary()
+
 model.fit(
-    videos_buffer,
+    data, data,
+    batch_size=32,
     epochs=epochs,
     callbacks=callback_list
 )
--- batch_data_generator.py
+++ batch_data_generator.py
@@ -1,8 +1,18 @@
 """
 https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
 """
+import math
 
 class BatchDataGenerator:
 
-    def __init__(self):
-        pass
+    def __init__(self, images, batch_size):
+        self.images = images
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return math.ceil(len(self.images) / self.batch_size)
+
+    def __getitem__(self, idx):
+        images_batch = self.images[idx * self.batch_size:(idx + 1) *
+        self.batch_size]
+        return images_batch
